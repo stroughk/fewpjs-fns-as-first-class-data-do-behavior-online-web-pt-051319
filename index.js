29de9f232a9c79608f799f9f394cac331e21a975
Kateryna Strough <katerynastrough@hotmail.com>
2019-11-19 23:35:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(timeStr) {
+  const hour = parseInt(timeStr, 10);
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
